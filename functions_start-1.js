c7ff09c016a39a1735ec1de8766def2c22d23e8d
Maxfield B Mullins <mm61931@srcs.loc>
2017-02-09 14:12:31 -0800

    fixed combinations

--- functions_start-1.js
+++ functions_start-1.js
@@ -71,7 +71,7 @@ function factorial(n) {
  */
 function combinations(n, k) {
     var c;
-    c = factorial(n) / factorial(k) * (factorial(n - k));
+    c = factorial(n) / (factorial(k) * (factorial(n - k)));
     return c;
 }
 /**
